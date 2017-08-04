119b7175feda14a1c02619a63efd2dd51339cc92
Mr-Jemp <1303232158@qq.com>
2017-08-04 08:38:46 +0800

    v1.3

--- MIUI/移动端/js/index.js
+++ MIUI/移动端/js/index.js
@@ -1,15 +1,25 @@
 $(function(){
-	/*--------轮播--------*/
-	var swiper = new Swiper('.swiper-container', {
-	    pagination: '.swiper-pagination',
-	    slidesPerView: 1,
-	    paginationClickable: true,
-	    spaceBetween: 30,
-	    loop: true,
-	    autoplay:2000
-	});
+	/*--------swiper初始化轮播--------*/
+	function swiper(){
+		var swiper = new Swiper('.show .swiper-container', {
+		    pagination: '.swiper-pagination',
+		    spaceBetween: 30,
+		    loop: true,
+		    autoplay:2000
+		});
+	}
+	swiper();
 	/*--------轮播结束--------*/
 	
+	
+	/*----------首页导航页切换-----------*/
+	$("#header .nav li").tap(function(){
+		$(this).children("span").addClass("active");
+		$(this).siblings().children("span").removeClass("active");
+		$(".pages").eq($(this).index()).addClass("show").siblings().removeClass("show");
+		swiper();
+	})
+	/*----------首页导航页切换结束-----------*/
 	
 	
 	
@@ -18,4 +28,27 @@ $(function(){
 		$(".wraper").eq($(this).index()).addClass("dis").siblings().removeClass("dis");
 		$(this).addClass("active").siblings().removeClass("active")
 	})
+	/*------------固定导航------------*/
+	
+	
+	/*跳转登录页面*/
+	$("#index #login").tap(function(){
+		window.location.href = "login.html";
+	})
+	$("#shopping .go-login").tap(function(){
+		window.location.href = "login.html";
+	})
+	$("#personal .login-text").tap(function(){
+		window.location.href = "login.html";
+	})
+	
+	
+	
+	/*-----分类页侧边导航跳转对应标题-----*/
+	$("#classify .left-nav li").tap(function(){
+		$(this).addClass("active").siblings().removeClass("active");
+		$("html,body").scrollTop($(this).index()*$("#classify .right-content section").height());
+	})
+	
+	
 })
